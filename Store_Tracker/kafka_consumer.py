dd704e10a8ddd739d3d8df2ede8a2f70b5b2f237
Jeff Everett <everett.jeff.w@gmail.com>
2017-04-17 15:46:25 -0600

    Fixed indentation issue

--- Store_Tracker/kafka_consumer.py
+++ Store_Tracker/kafka_consumer.py
@@ -17,39 +17,39 @@ pi_locations = {}
 
 consumer = KafkaConsumer(TOPIC, bootstrap_servers=SERVERS, auto_offset_reset='earliest', group_id=GROUP_ID)
 for msg in consumer:
-	data = json.loads(msg[6])
+    data = json.loads(msg[6])
 
-	# write pi-specific properties to dictionary, overwriting if necessary
-	tx_powers[data['pi_id']] = data['tx_powers']
-	pi_locations[data['pi_id']] = data['pi_locations']
+    # write pi-specific properties to dictionary, overwriting if necessary
+    tx_powers[data['pi_id']] = data['tx_powers']
+    pi_locations[data['pi_id']] = data['pi_locations']
 
-	# append rssi reading to list if list for pi id exists, otherwise create one
+    # append rssi reading to list if list for pi id exists, otherwise create one
     if data['pi_id'] in readings:
-    	readings[data['pi_id']].append(data['rssi_reading'])
+        readings[data['pi_id']].append(data['rssi_reading'])
     else
-    	readings[data['pi_id']] = [data['rssi_reading']]
+        readings[data['pi_id']] = [data['rssi_reading']]
 
     # do actual processing after every time window elapses
     if data['timestamp'] - last_window_timestamp >= TIME_WINDOW:
-    	median_distances = []
-    	for pi_id in readings:
-    		distances = []
-    		for rssi_reading in data[pi_id]:
-    			# the formula used to compute the distances is 10^((TxPower-RSSI)/20)
-    			# it is derived from a formula which appears in the following paper:
-    			# http://www.rn.inf.tu-dresden.de/dargie/papers/icwcuca.pdf
-    			distances.append(10^(tx_powers[pi_id]-rssi_reading)/20)
-    		median_distance = statistics.median(distances)
-    		median_distances.append((pi_id, median_distance))
-    	median_distances.sort(key=lambda x: x[1])
+        median_distances = []
+        for pi_id in readings:
+            distances = []
+            for rssi_reading in data[pi_id]:
+                # the formula used to compute the distances is 10^((TxPower-RSSI)/20)
+                # it is derived from a formula which appears in the following paper:
+                # http://www.rn.inf.tu-dresden.de/dargie/papers/icwcuca.pdf
+                distances.append(10^(tx_powers[pi_id]-rssi_reading)/20)
+            median_distance = statistics.median(distances)
+            median_distances.append((pi_id, median_distance))
+        median_distances.sort(key=lambda x: x[1])
 
-    	# use trilateration on three pis with smallest median distances
-    	circle_list = [
-    		trilateration.circle(pi_locations[median_distances[0][0]], median_distances[0][1]),
-    		trilateration.circle(pi_locations[median_distances[1][0]], median_distances[1][1]),
-    		trilateration.circle(pi_locations[median_distances[2][0]], median_distances[2][1])]
-    	computed_location = trilateration.do_trilateration(circle_list)
+        # use trilateration on three pis with smallest median distances
+        circle_list = [
+            trilateration.circle(pi_locations[median_distances[0][0]], median_distances[0][1]),
+            trilateration.circle(pi_locations[median_distances[1][0]], median_distances[1][1]),
+            trilateration.circle(pi_locations[median_distances[2][0]], median_distances[2][1])]
+        computed_location = trilateration.do_trilateration(circle_list)
 
-    	print(computed_location.x, computed_location.y)
+        print(computed_location.x, computed_location.y)
 
 consumer.close()
