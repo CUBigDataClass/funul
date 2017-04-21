8c9a123066b10dde6eed1a798cd3c098d2ddf965
Jeff Everett <everett.jeff.w@gmail.com>
2017-04-20 22:30:12 -0600

    Connected Kafka producer to CouchDB changes feed

--- Store_Tracker/kafka_producer.py
+++ Store_Tracker/kafka_producer.py
@@ -1,23 +1,24 @@
 from kafka import KafkaProducer
 from random import randint
+import couchdb
 import trilateration
 import json
 import time
 
-SERVERS = ['localhost:9092']
+SERVERS = ['http://13.58.127.253:9092']
 TOPIC= 'bluetooth_readings' # topic to be used for all trilateration procedures
 ACKS = 0 # don't require any acknowledgements to consider the request complete
-PI_ID = 'mypi3' # replace with unique id for each pi -- either using a command/libary or hard-coding
-PI_LOCATION = trilateration.point(40, 10)
+COUCHDB_SERVER = 'http://52.53.196.80:5984'
 
+server = couchdb.client.Server(COUCHDB_SERVER)
+db = server['ble']
 producer = KafkaProducer(bootstrap_servers=SERVERS, acks=ACKS)
 
-for i in range(100):
+for change in db.changes(feed='continuous'):
+    doc = db.get(change.id)
     producer.send(TOPIC, value=json.dumps({
-        'pi_id': PI_ID,
-        'pi_location': PI_LOCATION
-        'tx_power': 40,
-        'rssi_reading': randint(-90, -30),
-        'timestamp': time.time()
-    }), key='BLUETOOTH1')
+        'pi_id': doc['pi_id'],
+        'rssi_reading': doc['rssi_reading'],
+        'timestamp': doc['timestamp']}), key='BLUETOOTH1')
+
 producer.close()
