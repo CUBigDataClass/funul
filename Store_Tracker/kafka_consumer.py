9891602735f18675c13912984139bc488b180253
Jeff Everett <everett.jeff.w@gmail.com>
2017-04-20 23:06:34 -0600

    Add processed data to separate CouchDB database

--- Store_Tracker/kafka_consumer.py
+++ Store_Tracker/kafka_consumer.py
@@ -1,19 +1,26 @@
 from kafka import KafkaConsumer
+import couchdb
 import time
 import statistics
 import json
 import trilateration
 
-SERVERS = ['localhost:9092'] # kafka server list
+SERVERS = ['http://13.58.127.253:9092'] # kafka server list
 TOPIC = 'bluetooth_readings' # topic to be used for all trilateration procedures
 GROUP_ID = 'test_group'
 TIME_WINDOW = 5 # in seconds
 TX_POWER = -65 # power received by receiver at distance of 1m
+COUCHDB_SERVER = 'http://52.53.196.80:5984'
 
 
 # start new reading every 3 seconds
 last_window_timestamp = time.time()
 readings = {}
+
+# the pi locations would ideally be set on the actual pis and then stored in the database.
+# we are manually entering them here for demo purposes. it is time-consuming to manually
+# change these values on multiple devices, especially on the school network where we cannot
+# give them static ips (which also makes automation difficult)
 pi_locations = {
     'pi_1': trilateration.point(0,0),
     'pi_2': trilateration.point(10,0),
@@ -44,7 +51,7 @@ for msg in consumer:
                 # the formula used to compute the distances is 10^((TxPower-RSSI)/20)
                 # it is derived from a formula which appears in the following paper:
                 # http://www.rn.inf.tu-dresden.de/dargie/papers/icwcuca.pdf
-                distances.append(10^(tx_powers[pi_id]-rssi_reading)/20)
+                distances.append(10^(TX_POWER-rssi_reading)/20)
             median_distance = statistics.median(distances)
             median_distances.append((pi_id, median_distance))
         median_distances.sort(key=lambda x: x[1])
@@ -58,6 +65,8 @@ for msg in consumer:
             trilateration.circle(pi_locations[median_distances[1][0]], median_distances[1][1]),
             trilateration.circle(pi_locations[median_distances[2][0]], median_distances[2][1])]
         computed_location = trilateration.do_trilateration(circle_list)
+
+        server = couchdb.client.Server(COUCHDB_SERVER)
 
         print(computed_location.x, computed_location.y)
 
