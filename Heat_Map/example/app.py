dea4c8e69c5b1156accf2e191602f6df8a588a45
Brandon <brsp7274@colorado.edu>
2017-04-23 15:29:39 -0600

    added front end to flask

--- Heat_Map/example/app.py
+++ Heat_Map/example/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, url_for
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
 from random import random
 import couchdb
+from flask_bootstrap import Bootstrap
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
@@ -13,8 +14,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
+Bootstrap(app)
 
-'''
 def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
@@ -41,8 +42,8 @@ def background_thread():
 		socketio.emit('newnumber',{'x':doc['location_x'],'y':doc['location_y']})
 		if not thread_stop_event.isSet():
 			break
-
-
+'''
+#url_for('static', filename='style.css')
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
---
+++ Heat_Map/example/templates/js/login.js
@@ -0,0 +1,65 @@
+(function() {
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyBpkRAES6x70xhygiQAg_lN6dv2nlCAw0k",
+    authDomain: "funul-8cd90.firebaseapp.com",
+    databaseURL: "https://funul-8cd90.firebaseio.com",
+    projectId: "funul-8cd90",
+    storageBucket: "funul-8cd90.appspot.com",
+    messagingSenderId: "1046205384342"
+  };
+  firebase.initializeApp(config);
+
+  const txtEmail = document.getElementById('txtEmail');
+	const txtPassword = document.getElementById('txtPassword');
+	const btnLogin = document.getElementById('btnLogin');
+	const btnSignUp = document.getElementById('btnSignUp');
+	const loginForm = document.getElementById('login_form');
+  const btnNavLogin = document.getElementById('navLogin');
+  const btnSignOut = document.getElementById('navSignOut');
+  const splashDiv = document.getElementById('splash');
+
+  //add login event
+	btnLogin.addEventListener('click', e => {
+		const email = txtEmail.value;
+		const pass = txtPassword.value;
+		const auth = firebase.auth();
+		//sign in
+		const promise = auth.signInWithEmailAndPassword(email, pass);
+		promise.catch(e => alert(e.message));
+    $("#myModal").modal("hide");
+	});
+
+	//sign up
+	btnSignUp.addEventListener('click', e =>{
+		const email = txtEmail.value;
+		const pass = txtPassword.value;
+		const auth = firebase.auth();
+		//sign in
+		const promise = auth.createUserWithEmailAndPassword(email, pass);
+		promise.catch(e => alert(e.message));
+    $("#myModal").modal("hide");
+	});
+
+  //sign out
+	btnSignOut.addEventListener('click', e => {
+		firebase.auth().signOut();
+	});
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var email = user.email;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      btnNavLogin.classList.add('hide');
+      btnSignOut.classList.remove('hide');
+      splashDiv.classList.add("hide");
+    } else {
+      btnNavLogin.classList.remove('hide');
+      btnSignOut.classList.add('hide');
+      splashDiv.classList.remove("hide");
+    }
+  });
+
+}());
