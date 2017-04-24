91540805fcee11d7d48d74198a5553bf8b52f5ee
Tanner Quigley <TannerQuigley@me.com>
2017-04-23 18:53:21 -0600

    automated email is now sent to firebase user

--- email_receipt/funul_automated_email.py
+++ email_receipt/funul_automated_email.py
@@ -1,9 +1,13 @@
 import smtplib
+from firebase import firebase
+
+fire = firebase.FirebaseApplication('https://funul-8cd90.firebaseio.com/', None)
+result = fire.get('/users', '/email')
 
 smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
 smtpserver.ehlo()
 smtpserver.login('drew.meyers@funul.io', 'drewmeyers#1')
-smtpserver.sendmail('drew.meyers@funul.io', 'brandon.spitler@colorado.edu', 'wassup dude')
+smtpserver.sendmail('drew.meyers@funul.io', result, 'wassup dude')
 smtpserver.close()
