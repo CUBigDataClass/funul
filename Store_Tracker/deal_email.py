668a1e22edfc0b207b376405f6a882bb7c59e07d
Jeff Everett <everett.jeff.w@gmail.com>
2017-04-24 23:23:35 -0600

    debugging script

--- Store_Tracker/deal_email.py
+++ Store_Tracker/deal_email.py
@@ -9,14 +9,14 @@ import email.MIMEText# import MIMEtext
 
 def sendDealMail():
 	#tabl_tab = tablulate
-	fire = firebase.FirebaseApplication('https://funul-8cd90.firebaseio.com/', None)
-	result = fire.get('/users', '/email')
+    fire = firebase.FirebaseApplication('https://funul-8cd90.firebaseio.com/', None)
+    result = fire.get('/users', '/email')
 
-	smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
-	smtpserver.ehlo()
-	smtpserver.starttls()
-	smtpserver.ehlo()
-	smtpserver.login('drew.meyers@funul.io', 'drewmeyers#1')
-    	text = 'Looking for some milk? We have a great deal on gallons of Organic Valley right now, 50% for the next hour!!'
-    	smtpserver.sendmail('drew.meyers@funul.io', result, text)
-    	smtpserver.close()
+    smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo()
+    smtpserver.login('drew.meyers@funul.io', 'drewmeyers#1')
+    text = 'Looking for some milk? We have a great deal on gallons of Organic Valley right now, 50% for the next hour!!'
+    smtpserver.sendmail('drew.meyers@funul.io', result, text)
+    smtpserver.close()
